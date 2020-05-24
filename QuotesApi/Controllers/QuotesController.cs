e45c8c5b425b6a8be92b06bd5a6f42f1c7858a06
Cesar Lopez <clopezlerma@wayfair.com>
2020-05-24 15:57:32 -0600

    First commit

---
+++ QuotesApi/Controllers/QuotesController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using QuotesApi.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace QuotesApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class QuotesController : Controller
+    {
+
+        private static List<Quote> Quotes = new List<Quote>()
+        {
+            new Quote(){Id=0, Title="Imagination", Author="Cesar", Description="Imagination is more important than knowledge."},
+            new Quote(){Id=1, Title="Alive in morning", Author="Omar", Description="Thank you for being alive."}
+        };
+
+
+        [HttpGet]
+        public IEnumerable<Quote> Get()
+        {
+            return Quotes;
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Quote model)
+        {
+            if(model != null)
+            {
+                Quotes.Add(model);
+
+                return CreatedAtAction(nameof(Post), new { Id = model.Id, Message = "Created" });
+
+            }
+
+            return BadRequest();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Quote model)
+        {
+            if( id > 0 && model != null)
+            {
+                var idx = Quotes.FindIndex(x => x.Id == id);
+
+                if(idx >= 0)
+                {
+                    Quotes[idx] = model;
+
+                    return Ok(new { Id = id, Message = "Updated" });
+
+                }
+
+                return NotFound(new { Id = id, Message = "Not Found" });
+
+            }
+
+            return BadRequest();
+
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            if(id > 0)
+            {
+                var idx = Quotes.FindIndex(x => x.Id == id);
+
+                if (idx >= 0)
+                {
+                    Quotes.RemoveAt(idx);
+
+                    return Ok(new { Id = id, Message = "Deleted" });
+                }
+
+                return NotFound(new { Id = id });
+            }
+
+            return BadRequest();
+        }
+
+    }
+
+}
